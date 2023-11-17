0a95194d076f5ab45e606593e52253561ad7100b
Thomas Camminady <0milieux_member@icloud.com>
2023-11-17 09:25:47 +0100

    Fix typing issue

--- adventofcode/helper/template/copy_files.py
+++ adventofcode/helper/template/copy_files.py
@@ -1,10 +1,9 @@
 import os
 import shutil
 
-basefile = "adventofcode/helper/template/00.py"
 for i in range(1, 25):
     dir = "adventofcode/"
     filename = os.path.join(str(i).zfill(2) + ".py")
     fullpath = os.path.join(dir, filename)
     if not os.path.exists(fullpath):
-        shutil.copy(basefile, fullpath)
+        shutil.copy("adventofcode/helper/template/00.py", fullpath)
