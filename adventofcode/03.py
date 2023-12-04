9c56829d322da02597084a22f95bd971bade344a
Thomas Camminady <thomascamminady@Thomass-MacBook-Air.local>
2023-12-04 10:23:45 -0700

    Day 04.

--- adventofcode/03.py
+++ adventofcode/03.py
@@ -2,14 +2,64 @@ from __future__ import annotations
 
 from rich import print
 import fire
+import numpy as np
 from adventofcode.helper.io import get_day, get_riddle_input, save_riddle_input
+import re
+
+
+def extract_numbers_with_positions(text):
+    # Regular expression to find numbers and their positions
+    matches = re.finditer(r"\d+", text)
+    result = [(int(match.group()), match.start()) for match in matches]
+    return result
+
+
+def is_special(c):
+    if c == "." or c in [str(i) for i in range(10)]:
+        return False
+    return True
+
+
+def is_okay(i, j):
+    _max = 139
+    if i < 0 or i > _max or j < 0 or j > _max:
+        return False
+    return True
 
 
 def riddle1(riddle_input: str) -> int | str:
     answer = 0
 
+    y = []
+    z = []
     for i, line in enumerate(riddle_input.splitlines()):
-        pass
+        x = extract_numbers_with_positions(line)
+
+        y.append(x)
+        z.append([_ for _ in line])
+    M = np.array(z, dtype=str)
+
+    for row, dummy in enumerate(y):
+        for number, position in dummy:
+            k = len(str(number))
+
+            surroundings = []
+            left = (row, position - 1)
+            right = (row, position + k)
+            above = [(row - 1, _) for _ in range(position - 1, position + k + 1)]
+            below = [(row + 1, _) for _ in range(position - 1, position + k + 1)]
+            surroundings.append(left)
+            surroundings.append(right)
+            surroundings.extend(above)
+            surroundings.extend(below)
+            found_any = False
+            for s in surroundings:
+                if is_okay(s[0], s[1]):
+                    if is_special(M[s[0], s[1]]):
+                        found_any = True
+
+            if found_any:
+                answer += int(number)
 
     return answer
 
@@ -17,8 +67,43 @@ def riddle1(riddle_input: str) -> int | str:
 def riddle2(riddle_input: str) -> int | str:
     answer = 0
 
+    y = []
+    z = []
     for i, line in enumerate(riddle_input.splitlines()):
-        pass
+        x = extract_numbers_with_positions(line)
+
+        y.append(x)
+        z.append([_ for _ in line])
+    M = np.array(z, dtype=str)
+
+    count = np.zeros((140, 140))
+    first = np.zeros((140, 140))
+    second = np.zeros((140, 140))
+
+    for row, dummy in enumerate(y):
+        for number, position in dummy:
+            k = len(str(number))
+
+            surroundings = []
+            left = (row, position - 1)
+            right = (row, position + k)
+            above = [(row - 1, _) for _ in range(position - 1, position + k + 1)]
+            below = [(row + 1, _) for _ in range(position - 1, position + k + 1)]
+            surroundings.append(left)
+            surroundings.append(right)
+            surroundings.extend(above)
+            surroundings.extend(below)
+            for s in surroundings:
+                i, j = s
+                if is_okay(i, j):
+                    if M[i, j] == "*":
+                        count[i, j] += 1
+                        if first[i, j] == 0:
+                            first[i, j] = number
+                        else:
+                            second[i, j] = number
+
+    return int(np.sum((count > 1) * first * second))
 
     return answer
 
--- adventofcode/04.py
+++ adventofcode/04.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from collections import defaultdict
 
 from rich import print
 import fire
@@ -9,18 +10,49 @@ def riddle1(riddle_input: str) -> int | str:
     answer = 0
 
     for i, line in enumerate(riddle_input.splitlines()):
-        pass
+        yours = [int(_) for _ in line.split(":")[1].split("|")[0].strip().split()]
+        wins = [int(_) for _ in line.split(":")[1].split("|")[1].strip().split()]
 
+        is_good = [1 if _ in wins else 0 for _ in yours]
+        n = sum(is_good)
+        if n == 0:
+            answer += 0
+        else:
+            answer += 2 ** (n - 1)
     return answer
 
 
 def riddle2(riddle_input: str) -> int | str:
     answer = 0
 
-    for i, line in enumerate(riddle_input.splitlines()):
-        pass
+    def get_score(line):
+        yours = [int(_) for _ in line.split(":")[1].split("|")[0].strip().split()]
+        wins = [int(_) for _ in line.split(":")[1].split("|")[1].strip().split()]
 
-    return answer
+        is_good = [1 if _ in wins else 0 for _ in yours]
+        n = sum(is_good)
+        return n
+
+    wins = {i: get_score(line) for i, line in enumerate(riddle_input.splitlines())}
+    inventory = defaultdict(int)
+    for i in range(0, len(riddle_input.splitlines())):
+        inventory[i] = 1
+    active = 0
+    original_inventory = {}
+    while True:
+        if inventory[active] == 0 and inventory[active + 1] == 0:
+            break
+        elif inventory[active] == 0:
+            active += 1
+        else:
+            n = wins[active]
+            original_inventory[active] = inventory[active]
+
+            # print("here", n, inventory[active], active)
+            for _ in range(n):
+                inventory[active + _ + 1] += inventory[active]
+            inventory[active] = 0
+    return sum([v for k, v in original_inventory.items()])
 
 
 def aoc(show: bool = False, save: bool = True):
@@ -37,6 +69,12 @@ def aoc(show: bool = False, save: bool = True):
     print(answer1)
 
     if answer1 != 0:
+        #         riddle_input = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        # Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+        # Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+        # Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+        # Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+        # Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"""
         answer2 = riddle2(riddle_input)
         print(answer2)
 
