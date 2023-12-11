a4642063777ad827cbb3858c0da999e29b171364
Thomas Camminady <thomascamminady@Thomass-MacBook-Air.local>
2023-12-11 09:22:22 +0100

    Day 11.

--- adventofcode/10.py
+++ adventofcode/10.py
@@ -3,17 +3,79 @@ from __future__ import annotations
 import logging
 import fire
 from adventofcode.helper.io import get_day, get_riddle_input, save_riddle_input
-
+import networkx as nx
 
 EXAMPLE = """"""
+
+
+# | is a vertical pipe connecting north and south.
+# - is a horizontal pipe connecting east and west.
+# L is a 90-degree bend connecting north and east.
+# J is a 90-degree bend connecting north and west.
+# 7 is a 90-degree bend connecting south and west.
+# F is a 90-degree bend connecting south and east.
+# . is ground; there is no pipe in this tile.
+# S is the starting position of the animal; there is a pipe on this tile, but your sketch doesn't show what shape the pipe has.
+
+
+def ij2idx(i, j):
+    return i * 140 + j
 
 
 def riddle1(riddle_input: str) -> int | str:
     answer = 0
 
-    for i, line in enumerate(riddle_input.splitlines()):
-        pass
+    edges = []
+    starti, startj = 0, 0
 
+    charts = [[_ for _ in line] for line in riddle_input.splitlines()]
+    n = len(charts)
+    for i in range(n):
+        for j in range(n):
+            c = charts[i][j]
+            if c == ".":
+                continue
+            elif c == "|":
+                edges.append((ij2idx(i - 1, j), ij2idx(i, j)))
+                edges.append((ij2idx(i, j), ij2idx(i + 1, j)))
+            elif c == "-":
+                edges.append((ij2idx(i, j - 1), ij2idx(i, j)))
+                edges.append((ij2idx(i, j), ij2idx(i, j + 1)))
+            elif c == "L":
+                edges.append((ij2idx(i - 1, j), ij2idx(i, j)))
+                edges.append((ij2idx(i, j), ij2idx(i, j + 1)))
+            elif c == "J":
+                edges.append((ij2idx(i - 1, j), ij2idx(i, j)))
+                edges.append((ij2idx(i, j), ij2idx(i, j - 1)))
+            elif c == "7":
+                edges.append((ij2idx(i, j - 1), ij2idx(i, j)))
+                edges.append((ij2idx(i, j), ij2idx(i + 1, j)))
+            elif c == "F":
+                edges.append((ij2idx(i, j + 1), ij2idx(i, j)))
+                edges.append((ij2idx(i, j), ij2idx(i + 1, j)))
+            elif c == "S":
+                starti, startj = i, j
+
+    if charts[starti - 1][startj + 0] in (["|", "7", "F"]):
+        print("down")
+        edges.append((ij2idx(starti, startj), ij2idx(starti - 1, startj)))
+    if charts[starti + 1][startj + 0] in (["|", "L", "J"]):
+        print("up")
+        edges.append((ij2idx(starti, startj), ij2idx(starti + 1, startj)))
+    if charts[starti][startj - 1] in (["-", "F", "L"]):
+        print("left")
+        edges.append((ij2idx(starti, startj), ij2idx(starti, startj - 1)))
+    if charts[starti][startj + 1] in (["-", "7", "J"]):
+        print("right")
+        edges.append((ij2idx(starti, startj), ij2idx(starti, startj + 1)))
+
+    G = nx.from_edgelist(edges)
+    print(nx.find_cycle(G, ij2idx(starti, startj)))
+    return len(nx.find_cycle(G, ij2idx(starti, startj))) // 2
+
+    import matplotlib.pyplot as plt
+
+    plt.show()
     return answer
 
 
@@ -51,8 +113,8 @@ def aoc(
         print(answer2)
 
 
-def print(_):
-    logging.info(_)
+# def print(_):
+#     logging.info(_)
 
 
 if __name__ == "__main__":
