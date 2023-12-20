88ed34aac3a6f71792b6576a46a709465a513779
Thomas Camminady <0milieux_member@icloud.com>
2023-12-20 08:58:16 +0100

    Day 20.

--- adventofcode/20/main.py
+++ adventofcode/20/main.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from abc import ABC, abstractmethod
 
 import fire
 from adventofcode.helper.io import (
@@ -7,14 +8,82 @@ from adventofcode.helper.io import (
     get_riddle_input,
     save_riddle_input,
 )
+from rich import print
+
+
+class Module(ABC):
+    def __init__(self, name: str, connections: list[str]) -> None:
+        self.name = name
+        self.connections = connections
+        self.on = False
+
+    @abstractmethod
+    def receives(self, source: str, high_pulse: bool) -> list[tuple[str, bool]]:
+        pass
+
+
+class Broadcast(Module):
+    def receives(self, source: str, high_pulse: bool) -> list[tuple[str, bool]]:
+        return [(_, high_pulse) for _ in self.connections]
+
+
+class Conjunction(Module):
+    def __init__(self, name: str, connections: list[str]) -> None:
+        self.remembers_high = [False for _ in connections]
+        super().__init__(name, connections)
+
+    def receives(self, source: str, high_pulse: bool) -> list[tuple[str, bool]]:
+        for i in range(len(self.connections)):
+            if self.connections[i] == source:
+                self.remembers_high[i] = high_pulse
+
+        if all(self.remembers_high):
+            return [(_, False) for _ in self.connections]
+        else:
+            return [(_, True) for _ in self.connections]
+
+
+class FlipFlop(Module):
+    def receives(self, source: str, high_pulse: bool) -> list[tuple[str, bool]]:
+        if not high_pulse:
+            self.state = not self.state
+            return [(_, self.state) for _ in self.connections]
+        return []
+
+
+class ModuleFactory:
+    def __init__(self) -> None:
+        pass
+
+    def create(self, line) -> Module:
+        sign = line[0].strip()
+        name, connections = line[1:].split("->")
+        name = name.strip()
+        connections = [_.strip() for _ in connections.split(",")]
+        print(line, name, connections)
+        if sign == "b":
+            return Broadcast("broadcaster", connections)
+        elif sign == "%":
+            return FlipFlop(name, connections)
+        elif sign == "&":
+            return Conjunction(name, connections)
+        else:
+            raise ValueError
+
+
+def parse(riddle_input: str) -> dict[str, Module]:
+    mf = ModuleFactory()
+    modules = {}
+    for i, line in enumerate(riddle_input.splitlines()):
+        module = mf.create(line)
+        modules[module.name] = modules
+    return modules
 
 
 def riddle1(riddle_input: str) -> int | str:
     answer = 0
-
-    for i, line in enumerate(riddle_input.splitlines()):
-        pass
-
+    modules = parse(riddle_input)
+    print(modules)
     return answer
 
 
